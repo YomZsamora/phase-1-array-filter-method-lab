a64d7472a89ca922ffd3e90a7b05ac0dd5e2dc60
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 21:38:06 +0300

    Returns all drivers that match the passed in name even if case does not match but letters do.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Code your solution here
+// Returns all drivers that match the passed in name even if case does not match but letters do
+let findMatching = (listOfDrivers, searchedDriver) => {
+   return listOfDrivers.filter( driverName => {  return driverName.toLowerCase() === searchedDriver.toLowerCase() });
+}
