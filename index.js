c8518e68442c68836fa5d28ab42d19571bc89078
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 21:39:56 +0300

    Completed Lab.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // Returns all drivers that match the passed in name even if case does not match but letters do
 let findMatching = (listOfDrivers, searchedDriver) => {
    return listOfDrivers.filter( driverName => {  return driverName.toLowerCase() === searchedDriver.toLowerCase() });
